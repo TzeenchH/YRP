389ac59f2a7c93fd528a4b8718d7d675af002d8e
TzeenchH <zenv99@mail.ru>
2021-02-01 03:00:00 +0300

    WIP=Add core API

---
+++ YmlReaderAPI/Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using YmlReaderAPI.Models;
+
+namespace YmlReaderAPI.Controllers
+{
+	[ApiController]
+	[Route("Dashboard")]
+	public class DashboardController : Controller
+	{
+		public DashboardController()
+		{
+
+		}
+
+		[ActionName("AllYmls")]
+		[HttpGet("AllYmls")]
+		public IEnumerable<int> GetYmls()
+		{
+			int[] test = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+			return test;
+		}
+
+		[ActionName("YmlDoc")]
+		[HttpGet("YmlDoc")]
+		public IEnumerable<BaseModelTest> GetDocs()
+		{
+			var model = new BaseModelTest[] { new BaseModelTest() { Id = 1, Name = "Test", Surname = "Test1" } };
+			return model;
+		}
+	}
+}
